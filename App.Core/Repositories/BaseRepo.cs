8c6caf1bb55bee39d4e84dd9686b2475ca922456
marco sameh <marcosameh678@gmail.com>
2021-10-19 22:42:08 +0200

    dr

---
+++ App.Core/Repositories/BaseRepo.cs
@@ -0,0 +1,28 @@
+﻿using App.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Repositories
+{
+    public class BaseRepo<TEntity> where TEntity : class
+    {
+        private readonly Protein4allContext DbContext;
+
+        protected DbSet<TEntity> DbSet;
+        public BaseRepo(Protein4allContext protein4AllContext)
+        {
+            DbContext = protein4AllContext;
+            DbSet = DbContext.Set<TEntity>();
+        }
+        public IQueryable<TEntity> GetAll()
+        {
+            IQueryable<TEntity> entities = DbSet;
+            return entities;
+        }
+
+    }
+}
