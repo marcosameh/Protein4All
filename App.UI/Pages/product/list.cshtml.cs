8e652fc8ee89fd493631c25b8215c88575a2890b
marco sameh <marcosameh678@gmail.com>
2021-10-25 21:58:25 +0200

    all

--- App.UI/Pages/product/list.cshtml.cs
+++ App.UI/Pages/product/list.cshtml.cs
@@ -1,11 +1,13 @@
 using System;
-
+using System.Collections.Generic;
 using System.Linq;
 using App.Core.Managers;
 using App.Core.Models;
 
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace App.UI.Pages.product
 {
@@ -13,15 +15,22 @@ namespace App.UI.Pages.product
     public class listModel : PageModel
     {
         private readonly ProductManager _ProductManager;
-     
+        private readonly CategoryManager _categoryManager;
+
         public IQueryable<Product> Products { get; set; }
-        public listModel(ProductManager productManager)
+        public SelectList CategoryList { get; set; }
+        public IEnumerable<ProductCategory> Categories { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public int Id { get; set; }
+        public listModel(ProductManager productManager, CategoryManager categoryManager)
         {
             _ProductManager = productManager;
-            
+            _categoryManager = categoryManager;
         }
-        public void OnGet()
+        public void OnGet(int?Id)
         {
+            Categories = _categoryManager.GetAllCategories();
+            CategoryList = new SelectList(Categories, "Id", "Name");
             Products = _ProductManager.GetAllProduct();
             
 
