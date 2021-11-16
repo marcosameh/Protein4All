0e4250e050e2c0dde6c5e2ca4583a29b406e4397
Marco Sameh <marcosameh678@gmail.com>
2021-11-17 00:32:26 +0200

    Remove Zoom

--- App.UI/wwwroot/js/main.js
+++ App.UI/wwwroot/js/main.js
@@ -95,10 +95,10 @@
   });
 
 	// Product img zoom
-	var zoomMainProduct = document.getElementById('product-main-img');
-	if (zoomMainProduct) {
-		$('#product-main-img .product-preview').zoom();
-	}
+	//var zoomMainProduct = document.getElementById('product-main-img');
+	//if (zoomMainProduct) {
+	//	$('#product-main-img .product-preview').zoom();
+	//}
 
 	/////////////////////////////////////////
 
